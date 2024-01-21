68b79f33dd3f6460f31041bd9f78d2662a3f8b21
obalax <oluwaseyifasawe@gmail.com>
2024-01-21 01:13:28 +0100

    feat: Change basic page display

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders password checker', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/password checker/i);
   expect(linkElement).toBeInTheDocument();
 });
